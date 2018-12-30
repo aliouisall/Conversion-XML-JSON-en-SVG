6cac4082f2d702143005503604bb84b0dceca956
ibnibrahim <aliouibnibrahim@gmail.com>
2018-12-30 20:40:27 +0000

    Argparse au lieu de Optparse et lecture de l'input

--- XJ_Convertor.py
+++ XJ_Convertor.py
@@ -3,14 +3,19 @@
 
 def gestiontOpt():
 
-    from optparse import OptionParser
+    import argparse
 
-    parser = OptionParser()
-    parser.add_option("-f", "--file", dest="filename",
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", dest="filename",
                     help="permet de désigner un input de type fichier", metavar="FILE")
-    parser.add_option("-i", "--input", help="permet de dire si l'input est en xml ou en json")
-    parser.add_option("-o", "--output", dest="filename", help="permet de désigner le fichier de sortie", metavar="FILE")
-    parser.add_option("-t", "--trace", help="permet de dire si on veut afficher les traces")
-    parser.add_option("--http", help="permet de désigner un input en flux http")
+    parser.add_argument("-i", "--input", help="permet de dire si l'input est en xml ou en json")
+    parser.add_argument("-o", "--output", dest="filename", help="permet de désigner le fichier de sortie", metavar="FILE")
+    parser.add_argument("-t", "--trace", help="permet de dire si on veut afficher les traces")
+    parser.add_argument("--http", help="permet de désigner un input en flux http")
 
-    (options, args) = parser.parse_args()
+    args = parser.parse_args()
+    file = args.filename
+    content = open(file)
+    print(content.read())
+
+gestiontOpt()
