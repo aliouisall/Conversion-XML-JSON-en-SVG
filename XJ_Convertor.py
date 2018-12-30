ed33a3ecc4dedec591c24a2e4869c2c5c4330545
ibnibrahim <aliouibnibrahim@gmail.com>
2018-12-30 18:49:01 +0000

    Définition de la fonction de gestion des options

---
+++ XJ_Convertor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.5
+# -*-coding:Utf-8 -*
+
+def getionOpt():
+
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    parser.add_option("-f", "--file", dest="filename",
+                    help="permet de désigner un input de type fichier", metavar="FILE")
+    parser.add_option("-i", "--input", help="permet de dire si l'input est en xml ou en json")
+    parser.add_option("-o", "--output", dest="filename", help="permet de désigner le fichier de sortie", metavar="FILE")
+    parser.add_option("-t", "--trace", help="permet de dire si on veut afficher les traces")
+    parser.add_option("--http", help="permet de désigner un input en flux http")
+
+    (options, args) = parser.parse_args()
