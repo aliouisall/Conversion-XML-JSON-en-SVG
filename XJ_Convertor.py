d9261ca3d9133633c7935579344ac876c2c41aae
ibnibrahim <aliouibnibrahim@gmail.com>
2018-12-31 01:18:12 +0000

    Validation du fichier JSON

--- XJ_Convertor.py
+++ XJ_Convertor.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python3.5
 # -*-coding:Utf-8 -*
 
-def gestiontOpt():
+# Importation des modules nécessaires
+import json
+import argparse
 
-    import argparse
+# Définition de la fonction de validation
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", dest="filename",
-                    help="permet de désigner un input de type fichier", metavar="FILE")
-    parser.add_argument("-i", "--input", help="permet de dire si l'input est en xml ou en json")
-    parser.add_argument("-o", "--output", dest="filename", help="permet de désigner le fichier de sortie", metavar="FILE")
-    parser.add_argument("-t", "--trace", help="permet de dire si on veut afficher les traces")
-    parser.add_argument("--http", help="permet de désigner un input en flux http")
+def jsonValidator(content):
+    try:
+        jsonContent = json.load(content)
+        json.dumps(jsonContent)
+        print(jsonContent)
+        return True
+    except ValueError as error:
+        print("Invalid json : %s" %error)
+        return False
 
-    args = parser.parse_args()
-    file = args.filename
-    content = open(file)
-    print(content.read())
+# Définition des différentes options
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", dest="filename",
+                help="permet de désigner un input de type fichier", metavar="FILE")
+parser.add_argument("-i", "--input", help="permet de dire si l'input est en xml ou en json")
+parser.add_argument("-o", "--output", dest="filename", help="permet de désigner le fichier de sortie", metavar="FILE")
+parser.add_argument("-t", "--trace", help="permet de dire si on veut afficher les traces")
+parser.add_argument("--http", help="permet de désigner un input en flux http")
 
-gestiontOpt()
+args = parser.parse_args()
+file = args.filename
+content = open(file)
+jsonValidator(content)
+
