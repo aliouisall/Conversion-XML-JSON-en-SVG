81c8939276fec1da02275f84b8c341e56b46cdcb
ibnibrahim <aliouibnibrahim@gmail.com>
2019-01-07 23:09:34 +0000

    Découpage en fonction et argument output pris en compte

--- XJ_Convertor.py
+++ XJ_Convertor.py
@@ -7,44 +7,44 @@ import argparse
 from graphviz import Digraph
 
 dot = Digraph(comment='Entity Relationship Diagram')
-# dot.attr('node')
-# dot.node('A',  shape='record', label="{Aliou | Sall}")
-# dot.node('B', 'Sir Bedevere the Wise')
-# dot.node('L', 'Sir Lancelot the Brave')
-# print(dot.source)
-# dot.render('test-output/round-table.gv', view=True)
 
 # dot.edges(['AB', 'AL'])
 # dot.edge('B', 'L', constraint='false')
+
+# Déclaration du tableau des entités et de celui des attributs
+arrayEntities = []
+attributes = []
+
+# Définition de la fonction de génération
+def gener():
+    j = 0
+    k = 0
+    attribArrayLength = len(attributes)
+    while (k < len(arrayEntities)):
+        dot.node(arrayEntities[k],  shape='record', color='blue', label=arrayEntities[k])
+        while (j < attribArrayLength):
+            dict = list(attributes)[j]
+            for element in dict:
+                dot.node(element,  shape='record', color='blue', label=element)
+            print(dot.source)
+            j += 1
+        k += 1
+
+# Définition de la fonction d'extraction
+def jsonExtractor(parsedContent):
+    numbItems = len(parsedContent)
+    i = 2
+    while (i < numbItems):
+        arrayEntities.append(parsedContent[i]['name'])
+        attributes.append(parsedContent[i]['data'][0].keys())
+        i = i + 1
+    gener()
 
 # Définition de la fonction de validation
 def jsonValidator(content):
     try:
         parsedJson = json.load(content)
-        # jsonContent = json.dumps(parsedJson, sort_keys=True, indent = 4)
-        numbItems = len(parsedJson)
-        i = 2
-        arrayEntities = []
-        attributes = []
-        while (i < numbItems):
-            arrayEntities.append(parsedJson[i]['name'])
-            attributes.append(parsedJson[i]['data'][0].keys())
-            # print(i)
-            i = i + 1
-        # for item in arrayEntities:
-        #     print(item)
-        j = 0
-        k = 0
-        # l = 0
-        attribArrayLength = len(attributes)
-        while (k < len(arrayEntities)):
-            dot.node(arrayEntities[k],  shape='record', color='blue', label=arrayEntities[k])
-            while (j < attribArrayLength):
-                dict = list(attributes)[j]
-                for element in dict:
-                    dot.node(element,  shape='record', color='blue', label=element)
-                j += 1
-            k += 1
+        jsonExtractor(parsedJson)
         return True
     except ValueError as error:
         print("Error : %s" %error)
@@ -52,21 +52,18 @@ def jsonValidator(content):
 
 # Définition des différentes options et arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", dest="filename", help="permet de désigner un input de type fichier", metavar="FILE")
+parser.add_argument("-f", "--file", dest="inputFile", help="permet de désigner un input de type fichier", metavar="FILE")
 parser.add_argument("-i", "--input", help="permet de dire si l'input est en xml ou en json")
-parser.add_argument("-o", "--output", dest="filename", help="permet de désigner le fichier de sortie", metavar="FILE")
+parser.add_argument("-o", "--output", dest="outputFile", help="permet de désigner le fichier de sortie", metavar="FILE")
 parser.add_argument("-t", "--trace", help="permet de dire si on veut afficher les traces")
 parser.add_argument("--http", help="permet de désigner un input en flux http")
 
 args = parser.parse_args()
 
-# Récupération du fichier passé en argument
-file = args.filename
-
 # Ouverture du fichier
-content = open(file)
+content = open(args.inputFile)
 
 # Appel de la fonction de validation
 jsonValidator(content)
 dot.format = 'svg'
-dot.render('test-output/round-table.gv', view=True)
+dot.render(args.outputFile, view=True)
