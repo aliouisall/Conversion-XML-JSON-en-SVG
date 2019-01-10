331e58f11bf8e2012a8c56d8653765ae6a29e388
Mamadou Diop <diopmamadou706@gmail.com>
2019-01-10 20:40:16 +0000

    addition of another entity

--- Validation_Xml.py
+++ Validation_Xml.py
@@ -52,11 +52,22 @@ def ExtractElt(fich):
         id2 = attribut.find('id').text
         intitule = attribut.find('intitule').text
         cardi2 = entite2.find('cardinalite').text
+        cardi21 = entite2.find('cardinalite').text
+
+    for entite3 in root.findall('entite3'):
+        nomEntite3 = entite3.find('nomEntite').text
+
+    for attribut in entite3.findall('attribut'):
+        id3 = attribut.find('id').text
+        intitule = attribut.find('intitule').text
+        cardi3 = entite3.find('cardinalite').text
+
 
     for association in root.findall('association'):
         nomAssociation = association.find('nomAssos').text
     for attribut in association.findall('attribut'):
         NbrEtu = attribut.find('date').text
+
 #Creation du fichier svg
     dwg = svgwrite.Drawing('projet.svg')
 
@@ -73,9 +84,19 @@ def ExtractElt(fich):
     dwg.add(dwg.rect((10, 400), (200, 100), stroke=svgwrite.rgb(0, 0, 0, '%'), fill='blue'))
     dwg.add(dwg.text(nomEntite2, insert=(70, 420), fill='black', font_size='15px', font_weight="bold", font_family="Arial"))
     dwg.add(dwg.text(id2, insert=(30,450), stroke='none', fill=svgwrite.rgb(15, 15, 15, '%'), font_size='15px', font_weight="bold"))
+    dwg.add(dwg.text(cardi21, insert=(215,445), stroke='none', fill=svgwrite.rgb(15, 15, 15, '%'), font_size='15px', font_weight="bold"))
     dwg.add(dwg.text(intitule, insert=(30,470), stroke='none', fill=svgwrite.rgb(15, 15, 15, '%'), font_size='15px', font_weight="bold"))
     dwg.add(dwg.text(cardi2, insert=(65,395), stroke='none', fill=svgwrite.rgb(15, 15, 15, '%'), font_size='15px', font_weight="bold"))
     dwg.add(dwg.line((100, 110), (100, 400),  stroke=svgwrite.rgb(0,0,0, '%')))
+
+#Creation de la troisième entité
+    dwg.add(dwg.rect((500, 400), (200, 100), stroke=svgwrite.rgb(0, 0, 0, '%'), fill='blue'))
+    dwg.add(dwg.text(nomEntite3, insert=(550, 420), fill='black', font_size='15px', font_weight="bold", font_family="Arial"))
+    dwg.add(dwg.text(id3, insert=(520, 450), stroke='none', fill=svgwrite.rgb(15, 15, 15, '%'), font_size='15px', font_weight="bold"))
+    dwg.add(dwg.text(intitule, insert=(520,470), stroke='none', fill=svgwrite.rgb(15, 15, 15, '%'), font_size='15px', font_weight="bold"))
+    dwg.add(dwg.text(cardi3, insert=(470,445), stroke='none', fill=svgwrite.rgb(15, 15, 15, '%'), font_size='15px', font_weight="bold"))
+    dwg.add(dwg.line((500, 430), (700, 430),  stroke=svgwrite.rgb(0,0,0, '%')))
+    dwg.add(dwg.line((210, 450), (500, 450),  stroke=svgwrite.rgb(0,0,0, '%')))
 
 
 #Creation de l'association
