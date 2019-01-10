ccb200d4214072d753a210293dd306d5e7cc64c4
Mamadou Diop <diopmamadou706@gmail.com>
2019-01-10 16:15:52 +0000

    entity extraction and association

--- Validation_Xml.py
+++ Validation_Xml.py
@@ -10,6 +10,8 @@ import os.path
 
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import ParseError
+from xml.etree.ElementTree import parse
+import svgwrite
 
 # Définition de la fonction vérifiant léxtenxion du fichier choisi
 
@@ -19,6 +21,7 @@ def extensionValidator(fichier):
 		print("Veuillez choisir un fichier xml svp")
 	else:
 		return xmlValidator(fichier)
+
 		
 # Définition de la fonction de validation d'fichier 
 
@@ -29,3 +32,29 @@ def xmlValidator(fic):
 	except ParseError as error:
 		print("Fichier xml invalid")
 		return False 
+#Extraction des éléments 
+def ExtractElt(fich):
+    tree = ET.parse(fich)
+    root = tree.getroot()
+
+    for entite1 in root.findall('entite1'):
+        nomEntite = entite1.find('nomEntite').text
+    for attribut in entite1.findall('attribut'):
+        id1 = attribut.find('id').text
+        nom = attribut.find('nom').text
+        prenom = attribut.find('prenom').text
+        cardi1 = entite1.find('cardinalite').text
+
+    for entite2 in root.findall('entite2'):
+        nomEntite2 = entite2.find('nomEntite').text
+
+    for attribut in entite2.findall('attribut'):
+        id2 = attribut.find('id').text
+        intitule = attribut.find('intitule').text
+        cardi2 = entite2.find('cardinalite').text
+
+    for association in root.findall('association'):
+        nomAssociation = association.find('nomAssos').text
+    for attribut in association.findall('attribut'):
+        NbrEtu = attribut.find('date').text
+  		
