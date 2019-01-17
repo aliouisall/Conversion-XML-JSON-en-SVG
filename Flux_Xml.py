bc829dccb8dcb8bc7c4adf9a39254b55823fe774
Mamadou Diop <diopmamadou706@gmail.com>
2019-01-17 10:55:01 +0000

    display of entity

--- Flux_Xml.py
+++ Flux_Xml.py
@@ -4,9 +4,21 @@ import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import parse
 import requests
 import svgwrite
+#from XJ_Convertor import extractGener
+
 #Définition de la fonction qui accéde au fichier par flux pour recupérer son contenu
-def flux(url):
-	adress = requests.get(url)
+def flux():
+	adress = requests.get('http://sgbdproject.000webhostapp.com/files/kyQtZ.html')
 	fichier = open("flux.xml", "w")
 	fichier.write(adress.text)
 	fichier.close()
+	tree = ET.parse('Codification.xml')
+	root = tree.getroot()
+	for database in root.findall('database'):
+		for table in database.findall('table'):
+			name = table.get('name')
+			print(name)
+			tableContent = "{" + str(name)
+			for index, column in enumerate(table.findall('column')):
+				name = column.get('name')
+				print(name)
